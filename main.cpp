827944c57eb389001bffa898a3fc0fd6c6795f00
foxysatu <foxysatu232@gmail.com>
2022-06-11 16:09:13 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+    unsigned long long int ullAge = 18;// 64 bits:
+    signed long long int sllAge = 18;// 64 bits:
+
+    unsigned int Age = 18;          // 32 bits: 0.. ~4,293 billion.
+    signed int Weight = 100;        // 32 bits:  -2 billion..  +2 billion
+
+    unsigned short int ShAge = 18;  // 16 bits: 0.. 65535
+    signed short int ShWeight = 100;// 16 bits: -32768..+32767
+
+    unsigned char ChAge = 18;       // 8 bits: 0..255
+    signed char ChWeight = 100;     // 8 bits: -128..+127
+    char MyName[] = "";
+
+    float fAge = 18;                // 32 bits: 7-8 digits after point, -38..+37 power
+    double dfAge = 18;              // 64 bits: 15.16 digits after point, -308..+307 power
+    long double ldAge = 18;         // 80 bits:
+
+    bool    Z = true;
